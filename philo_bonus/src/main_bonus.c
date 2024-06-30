130361721b49810363581c7ff1a7c6c46fa820f3
ownleex <145456737+ownleex@users.noreply.github.com>
2024-07-01 01:07:17 +0200

    Refactor clean_up function for improved code organization and consistency

--- philo_bonus/src/main_bonus.c
+++ philo_bonus/src/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 22:38:21 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/07/01 00:30:37 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/07/01 00:58:07 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ static void	init_data(t_data *data, int argc, char **argv)
 	data->forks = sem_open("/forks", O_CREAT, 0644, data->num_philosophers);
 	data->print = sem_open("/print", O_CREAT, 0644, 1);
 	data->sem_alive = sem_open("/alive", O_CREAT, 0644, 1);
-	if (data->forks == SEM_FAILED || data->print == SEM_FAILED || data->sem_alive == SEM_FAILED)
+	if (data->forks == SEM_FAILED || data->print == SEM_FAILED || \
+	data->sem_alive == SEM_FAILED)
 	{
 		if (data->forks != SEM_FAILED)
 			sem_close(data->forks);
@@ -55,7 +56,8 @@ int	check_arg(char *str)
 
 int	check_valid_args(char **argv)
 {
-	if (ft_atoi(argv[1]) > MAX_PHILO || ft_atoi(argv[1]) <= 0 || check_arg(argv[1]) == 1)
+	if (ft_atoi(argv[1]) > MAX_PHILO || ft_atoi(argv[1]) <= 0 || \
+	check_arg(argv[1]) == 1)
 		return (write(2, "Invalid philosophers number\n", 29));
 	if (ft_atoi(argv[2]) <= 0 || check_arg(argv[2]) == 1)
 		return (write(2, "Invalid time to die\n", 21));
@@ -68,30 +70,30 @@ int	check_valid_args(char **argv)
 	return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_data data;
-    int i;
-    int status;
+	t_data	data;
+	int		i;
+	int		status;
 
-    if (argc != 5 && argc != 6)
-        return (write(2, "Wrong argument count\n", 22), 1);
-    if (check_valid_args(argv) > 0)
-        return (1);
-    init_data(&data, argc, argv);
-    start_simulation(&data);
-    i = -1;
-    while (++i < data.num_philosophers)
-    {
-        waitpid(-1, &status, 0);
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
-        {
-            i = -1;
-            while (++i < data.num_philosophers)
-                kill(data.philosophers[i].pid, SIGKILL);
-            break;
-        }
-    }
-    clean_up(&data);
-    return (0);
+	if (argc != 5 && argc != 6)
+		return (write(2, "Wrong argument count\n", 22), 1);
+	if (check_valid_args(argv) > 0)
+		return (1);
+	init_data(&data, argc, argv);
+	start_simulation(&data);
+	i = -1;
+	while (++i < data.num_philosophers)
+	{
+		waitpid(-1, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+		{
+			i = -1;
+			while (++i < data.num_philosophers)
+				kill(data.philosophers[i].pid, SIGKILL);
+			break ;
+		}
+	}
+	clean_up(&data);
+	return (0);
 }
--- philo_bonus/src/monitor_bonus.c
+++ philo_bonus/src/monitor_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 22:38:39 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/07/01 00:38:51 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/07/01 01:00:10 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,64 +33,62 @@ void	*monitor(void *arg)
 	return (NULL);
 }
 
-void philosopher_routine(t_philosopher *philo)
+void	philosopher_routine(t_philosopher *philo)
 {
-    pthread_t tid;
+	pthread_t	tid;
 
-    if (pthread_create(&tid, NULL, &monitor, philo) != 0)
-        exit(1);
-    pthread_detach(tid);  // Assurez-vous de détacher le thread moniteur si nécessaire
-
-    // Routine du philosophe
-    while (1)
-    {
-        // Le philosophe prend une fourchette
-        sem_wait(philo->data->forks);
-        sem_wait(philo->data->print);
-        printf("%lld %d has taken a fork\n", get_time() - philo->data->start_time, philo->id);
-        sem_post(philo->data->print);
-
-        // Le philosophe prend une autre fourchette et mange
-        sem_wait(philo->data->forks);
-        sem_wait(philo->data->print);
-        printf("%lld %d has taken a fork\n", get_time() - philo->data->start_time, philo->id);
-        sem_wait(philo->data->sem_alive);
-        printf("%lld %d is eating\n", get_time() - philo->data->start_time, philo->id);
-        philo->last_meal = get_time();
-        sem_post(philo->data->print);
-        ft_usleep(philo->data->time_to_eat);
-        sem_post(philo->data->sem_alive);
-        sem_post(philo->data->forks);
-        sem_post(philo->data->forks);
-
-        // Le philosophe dort
-        sem_wait(philo->data->print);
-        printf("%lld %d is sleeping\n", get_time() - philo->data->start_time, philo->id);
-        sem_post(philo->data->print);
-        ft_usleep(philo->data->time_to_sleep);
-
-        // Le philosophe pense
-        sem_wait(philo->data->print);
-        printf("%lld %d is thinking\n", get_time() - philo->data->start_time, philo->id);
-        sem_post(philo->data->print);
-    }
-    clean_up(philo->data);
+	if (pthread_create(&tid, NULL, &monitor, philo) != 0)
+		exit(1);
+	pthread_detach(tid);
+	while (1)
+	{
+		sem_wait(philo->data->forks);
+		sem_wait(philo->data->print);
+		printf("%lld %d has taken a fork\n", get_time() - \
+		philo->data->start_time, philo->id);
+		sem_post(philo->data->print);
+		sem_wait(philo->data->forks);
+		sem_wait(philo->data->print);
+		printf("%lld %d has taken a fork\n", get_time() - \
+		philo->data->start_time, philo->id);
+		sem_wait(philo->data->sem_alive);
+		printf("%lld %d is eating\n", get_time() - \
+		philo->data->start_time, philo->id);
+		philo->last_meal = get_time();
+		sem_post(philo->data->print);
+		ft_usleep(philo->data->time_to_eat);
+		sem_post(philo->data->sem_alive);
+		sem_post(philo->data->forks);
+		sem_post(philo->data->forks);
+		sem_wait(philo->data->print);
+		printf("%lld %d is sleeping\n", get_time() - \
+		philo->data->start_time, philo->id);
+		sem_post(philo->data->print);
+		ft_usleep(philo->data->time_to_sleep);
+		sem_wait(philo->data->print);
+		printf("%lld %d is thinking\n", get_time() - \
+		philo->data->start_time, philo->id);
+		sem_post(philo->data->print);
+	}
+	clean_up(philo->data);
 }
 
-void start_simulation(t_data *data)
+void	start_simulation(t_data *data)
 {
-    int i;
+	int	i;
 
-    for (i = 0; i < data->num_philosophers; i++)
-    {
-        data->philosophers[i].id = i + 1;
-        data->philosophers[i].data = data;
-        data->philosophers[i].last_meal = get_time();
-        data->philosophers[i].pid = fork();
-        if (data->philosophers[i].pid == 0)
-        {
-            philosopher_routine(&data->philosophers[i]);
-            exit(0);
-        }
-    }
+	i = 0;
+	while (i < data->num_philosophers)
+	{
+		data->philosophers[i].id = i + 1;
+		data->philosophers[i].data = data;
+		data->philosophers[i].last_meal = get_time();
+		data->philosophers[i].pid = fork();
+		if (data->philosophers[i].pid == 0)
+		{
+			philosopher_routine(&data->philosophers[i]);
+			exit(0);
+		}
+		i++;
+	}
 }
