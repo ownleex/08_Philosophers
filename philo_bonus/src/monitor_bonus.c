321770d5c094dd92657c043c51c44b49566b77f2
ownleex <145456737+ownleex@users.noreply.github.com>
2024-07-01 00:38:42 +0200

    chore: Refactor start_simulation function for improved code organization and consistency

--- philo_bonus/src/monitor_bonus.c
+++ philo_bonus/src/monitor_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 22:38:39 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/07/01 00:29:55 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/07/01 00:38:24 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,18 +77,21 @@ void philosopher_routine(t_philosopher *philo)
     clean_up(philo->data);
 }
 
-void	start_simulation(t_data *data)
+void start_simulation(t_data *data)
 {
-	int	i;
+    int i;
 
-	i = -1;
-	while (++i < data->num_philosophers)
-	{
-		data->philosophers[i].id = i + 1;
-		data->philosophers[i].data = data;
-		data->philosophers[i].last_meal = get_time();
-		data->philosophers[i].pid = fork();
-		if (data->philosophers[i].pid == 0)
-			philosopher_routine(&data->philosophers[i]);
-	}
+    for (i = 0; i < data->num_philosophers; i++)
+    {
+        data->philosophers[i].id = i + 1;
+        data->philosophers[i].data = data;
+        data->philosophers[i].last_meal = get_time();
+        data->philosophers[i].pid = fork();
+        if (data->philosophers[i].pid == 0)
+        {
+            philosopher_routine(&data->philosophers[i]);
+            exit(0);  // S'assurer que le processus enfant se termine correctement
+        }
+    }
 }
+
